b81c2a855776e7485abb5e24ca3171e34ebf5c11
The-Arkitekt <mr.davidcrumley@gmail.com>
2024-10-17 16:17:17 -0400

    updated to add flashprint

--- bash-config/bash_user_aliases.sh
+++ bash-config/bash_user_aliases.sh
@@ -1,4 +1,15 @@
+###########################
+# Basic Terminal Commands #
+###########################
+
 alias ls="ls --color=auto"
 alias grep="grep --color=auto"
 alias ll="ls -al"
 alias src="source ~/.bashrc"
+
+######################### 
+# Application Shortcuts #
+#########################
+
+alias freeCAD="flatpak run org.freecadweb.FreeCAD"
+alias flashPrint="flatpak run com.flashforge.FlashPrint"
